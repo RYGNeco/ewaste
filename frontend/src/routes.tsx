0e9cb77c33632c74092c6e3cf3ed79d3b2781b56
xyzarnav <arnav20604@gmail.com>
2025-07-25 17:04:54 +0530

    Refactor frontend structure: remove unused components, add global styles, and implement initial routing

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import App from './App';
+
+/**
+ * Application routes
+ * This file will be populated as we implement various pages
+ */
+export const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* Additional routes will be added here as components are developed */}
+    </Routes>
+  );
+};
+
+export default AppRoutes;
